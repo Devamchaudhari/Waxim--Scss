38d591abe4d604bfc27bf7dc98aafaab2134a103
Devamchaudhari <devamchaudhari40@gmail.com>
2023-08-04 17:13:08 +0530

    remove: logs | fix: blur of containers when menu open | fix: style of scroll arror when menu open

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,48 +1,74 @@
 var menu = document.getElementById(menuicon);
-// side menu function 
+// side menu function
 function sidemenu() {
-    console.log("my menu");
-    if (window.innerWidth < 632) {
-        console.log("less then", window.innerWidth);
-
-        document.getElementById("sidemenu").style.width = "100%";
-    } else {
-        console.log("gre then", window.innerWidth);
-
-        document.getElementById("sidemenu").style.width = "650px";
-    }
-    document.getElementById("sidemenu").style.height = "100%";
-    document.getElementById("menu-blur").style.filter = "blur(3px)";
-    document.getElementById("hero-container").style.filter = "blur(3px)";
-
+  if (window.innerWidth < 632) {
+    document.getElementById('sidemenu').style.width = '100%';
+  } else {
+    document.getElementById('sidemenu').style.width = '650px';
+  }
+  document.getElementById('scroll-up').classList.remove('arrow-scroll');
+  document.getElementById('sidemenu').style.height = '100%';
+  document.getElementById('menu-blur').style.filter = 'blur(3px)';
+  document.getElementById('hero-container').style.filter = 'blur(3px)';
+  document.getElementById('brand').style.filter = 'blur(3px)';
+  document.getElementById('info-container').style.filter = 'blur(3px)';
+  document.getElementById('digital-container').style.filter = 'blur(3px)';
+  document.getElementById('teams').style.filter = 'blur(3px)';
+  document.getElementById('portfolio').style.filter = 'blur(3px)';
+  document.getElementById('pricing').style.filter = 'blur(3px)';
+  document.getElementById('testimonal-container').style.filter = 'blur(3px)';
+  document.getElementById('mobile').style.filter = 'blur(3px)';
+  document.getElementById('design-container').style.filter = 'blur(3px)';
+  document.getElementById('dream').style.filter = 'blur(3px)';
+  document.getElementById('contact').style.filter = 'blur(3px)';
+  document.getElementById('google-map-container').style.filter = 'blur(3px)';
+  document.getElementById('footer-container').style.filter = 'blur(3px)';
 }
-// Background blur function 
+// Background blur function
 function closeNav() {
-    document.getElementById("sidemenu").style.width = "0";
-    document.getElementById("menu-blur").style.filter = "blur(0px)";
-    document.getElementById("hero-container").style.filter = "blur(0px)";
+  document.getElementById('scroll-up').classList.add('arrow-scroll');
+  document.getElementById('sidemenu').style.width = '0';
+  document.getElementById('menu-blur').style.filter = 'blur(0px)';
+  document.getElementById('hero-container').style.filter = 'blur(0px)';
+  document.getElementById('brand').style.filter = 'blur(0px)';
+  document.getElementById('info-container').style.filter = 'blur(0px)';
+  document.getElementById('digital-container').style.filter = 'blur(0px)';
+  document.getElementById('teams').style.filter = 'blur(0px)';
+  document.getElementById('portfolio').style.filter = 'blur(0px)';
+  document.getElementById('pricing').style.filter = 'blur(0px)';
+  document.getElementById('testimonal-container').style.filter = 'blur(0px)';
+  document.getElementById('mobile').style.filter = 'blur(0px)';
+  document.getElementById('design-container').style.filter = 'blur(0px)';
+  document.getElementById('dream').style.filter = 'blur(0px)';
+  document.getElementById('contact').style.filter = 'blur(0px)';
+  document.getElementById('google-map-container').style.filter = 'blur(0px)';
+  document.getElementById('footer-container').style.filter = 'blur(0px)';
 }
 
 // Scroll function
 
 window.addEventListener('scroll', function () {
-    let nav = document.getElementById("nav")
-    let scrollarrow = document.getElementById("scroll-up");
-    let windowscroll = window.scrollY > 400;
-    if (windowscroll) {
-        nav.classList.toggle('navbar-scroll', windowscroll);
-        scrollarrow.classList.toggle("arrow-scroll", windowscroll);
-    } else {
-        nav.classList.remove("navbar-scroll");
-        scrollarrow.classList.remove("arrow-scroll", windowscroll);
-    }
-})
+  let nav = document.getElementById('nav');
+  let scrollarrow = document.getElementById('scroll-up');
+  let windowscroll = window.scrollY > 400;
+  if (windowscroll) {
+    nav.classList.toggle('navbar-scroll', windowscroll);
+    scrollarrow.classList.toggle('arrow-scroll', windowscroll);
+  } else {
+    nav.classList.remove('navbar-scroll');
+    scrollarrow.classList.remove('arrow-scroll', windowscroll);
+  }
+});
 
 //arrow onclick function
 
-document.on('click', '.scroll-top-arrow', function () {
-    ('html, body').animate({
-        scrollTop: 0
-    }, 300);
-    return false;
+document.addEventListener('click', function (event) {
+  var target = event.target;
+  if (target.classList.contains('scroll-top-arrow')) {
+    event.preventDefault(); // Prevent default link behavior
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
 });
--- assets/script/map.js
+++ assets/script/map.js
@@ -1,12 +1,11 @@
 document.getElementById('map');
-console.log("hei");
 
 function MapmyIndia() {
-    var map = new MapmyIndia.Map("map", {
-        center: [28.61, 77.23],
-        zoomControl: true,
-        hybrid: true
-    });
-    console.log("map", map);
-    // var map = map.invalidateSize();
+  var map = new MapmyIndia.Map('map', {
+    center: [28.61, 77.23],
+    zoomControl: true,
+    hybrid: true,
+  });
+
+  // var map = map.invalidateSize();
 }
